2b9ac453296738240c2a35b7f0e81f90e337c9bf
Nikhil <nikhiljagatkari@gmail.com>
2025-06-28 15:33:46 +0530

    Fix import error: use react-toastify instead of react-hot-toast

--- src/components/ContactUsModal.jsx
+++ src/components/ContactUsModal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { toast } from 'react-hot-toast';
+import { toast } from 'react-toastify';
 
 const ContactUsModal = ({ userRole, onClose }) => {
   const [user, setUser] = useState(null);
