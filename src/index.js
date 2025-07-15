291ac6800dc6da6076daf6ae93eac99f88075d89
Nikhil <nikhiljagatkari@gmail.com>
2025-07-15 18:32:55 +0530

    web to app by WPA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
@@ -13,3 +14,6 @@ root.render(
 );
 
 reportWebVitals();
+
+// Register the service worker for PWA support
+serviceWorkerRegistration.register();
